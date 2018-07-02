00d6bfe73c7e435b968833a627c452ae545e41ef
Kenada-B <c.moon9@outlook.com>
2018-07-02 22:45:19 +0900

    first vue-messenger UI implement

---
+++ vue-messenger/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <table border="1" id="app-main">
+      <tr>
+        <td class="main-menu">
+          <router-link :to="{name:'friend-list'}"><font-awesome-icon icon="user" />친구목록</router-link>
+        </td>
+        <td class="main-menu">
+          <router-link :to="{name:'messageroom-list'}"><font-awesome-icon icon="comments" />대화목록</router-link>
+        </td>
+      <tr>
+        <td colspan="2">
+          <transition name="slide-fade" mode="out-in">
+            <router-view/>
+          </transition>    
+        </td>
+      </tr>
+    </table> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  #app-main{
+    border:1px solid #333333;
+    border-collapse: collapse;
+  }
+
+  .main-menu{
+    background:linear-gradient(to bottom, #666666, #333333, #000000);
+    text-align:center;
+    height:60px;
+    font-size:30px;
+    color:white;
+    width:300px;
+  }
+
+  .main-menu>a:link { color: white; text-decoration: none;}
+  .main-menu>a:visited { color: white; text-decoration: none;}
+  .main-menu>a:hover { color: white; text-decoration: none;}
+  .main-menu>a:active { color: #333333; text-decoration: none;}
+
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+
+  .slide-fade-enter, .slide-fade-leave-to
+  {
+    transform: translateX(10px);
+    opacity: 0;
+  }
+
+</style>
